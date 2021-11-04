5fc8b3e01887a6b8c93e909fcfa9cbd36c4ba06a
Spence Van Tiem <spvantiem6448@eagle.fgcu.edu>
2021-11-04 15:14:58 -0400

    Implementation of the Card class. Both the .h and .cpp files

--- BlackJackGame/Card.cpp
+++ BlackJackGame/Card.cpp
@@ -1,1 +1,67 @@
 #include "Card.h"
+#include <string>
+
+using namespace std;
+
+Card::Card() {
+	value = '0';
+	suit = '0';
+
+}
+
+Card::Card(char val, int st) {
+	value = val;
+	suit = st;
+
+	if (!isValid()) {
+		value = '0';
+		suit = '0';
+	}
+}
+
+Card::~Card() {
+
+}
+
+bool Card::isValid() {
+	return (((value>=50) && (value <= 57)) || (value == 'A') || (value == 'T') ||
+        (value == 'J') || (value == 'Q') || (value == 'K'))
+        && ((suit == 'C') || (suit == 'S') || (suit == 'H') || (suit == 'D'));
+}
+
+bool Card::setCard(char val, char st)
+{
+    value = val;
+    suit = st;
+
+    if (isValid())
+    {
+        return true;
+    }
+    else
+    {
+        value = '0';
+        suit = '0';
+        return false;
+    }
+}
+
+char Card::getValue()
+{
+    return value;
+}
+
+char Card::getSuit()
+{
+    return suit;
+}
+
+string Card::to_string()
+{
+    string name = "";
+    name += value;
+    name += suit;
+    return name;
+
+
+}
--- BlackJackGame/Card.h
+++ BlackJackGame/Card.h
@@ -1,6 +1,25 @@
 #pragma once
+#include <string>
+
+using namespace std;
+
 class Card
 {
 public:
+	Card();
+	Card(char val, int st);
+	virtual ~Card();
+
+	bool setCard(char val, char st); // Validating value and suit of the card
+	char getValue(); // Return card's value --> A2345678910JQK
+	char getSuit(); // Return card's suit --> Spade, Club, Heart, Diamond
+	string to_string(); // Return card as string --> AH, 10C, 4D ect.
+	bool isValid(); // Return true if card valid
+
+private:
+	char value; 
+	char suit;
+
+
 };
 
